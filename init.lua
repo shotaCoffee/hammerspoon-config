a82a7833a2ab817890c5ddc2d2e4126d635f84a9
shotaCoffee <dripcoffee1018@gmail.com>
2022-02-27 18:10:57 +0900

    init

---
+++ init.lua
@@ -0,0 +1,10 @@
+hs.loadSpoon("ShiftIt")
+spoon.ShiftIt:bindHotkeys({
+  left = {{'ctrl', 'cmd'}, 'left'},
+  right = {{'ctrl', 'cmd'}, 'right'},
+  up = {{'ctrl', 'cmd'}, 'up'},
+  down = {{'ctrl', 'cmd'}, 'down'},
+  center = {{'ctrl', 'cmd'}, 'c'},
+  resizeOut = {{ 'ctrl', 'cmd' }, '+' },
+  resizeIn = {{ 'ctrl', 'cmd' }, '-' }
+})
